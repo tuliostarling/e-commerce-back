68b9c33cf88670143d67dcbe17fc8629fbcead64
Caciquez <victorcaciquinho@gmail.com>
2018-11-11 19:33:32 -0200

    ' Shipping calcutation started'

--- controllers/shipping.js
+++ controllers/shipping.js
@@ -1,37 +1,23 @@
 'use strict';
 
 const Correio = require('node-correios');
-
+const shipInfo = require('../secrets/config');
 exports.getShipPrice = (req, res, callback) => {
-    
+    const { cep } = req.body;
+    const correios = new Correio();
+    shipInfo.correioConfig.sCepDestino = cep;
+
+    correios.calcPreco(shipInfo.correioConfig, (err, result) => {
+        console.log(err);
+        console.log(result);
+    });
+
 };
 
 exports.getShipInfo = (req, res, callback) => {
-
+    
+    //     correios.consultaCEP({ cep: '30140082' }, (err, result) => {
+    //         console.log(result);
+    //     })
 };
-
-// function toot() {
-//     let correios = new Correio();
-//     let obj = {
-//         nCdServico: '40010, 40045',
-//         sCepOrigem: '22041030',
-//         sCepDestino: '04569001',
-//         nVlPeso: 1,
-//         nCdFormato: 1,
-//         nVlComprimento: 20,
-//         nVlAltura: 4,
-//         nVlLargura: 11,
-//         nVlDiametro: 20,
-//         nVlValorDeclarado: 500
-//     };
-//     correios.calcPreco(obj, (err, result) => {
-//         console.log(err);
-//         console.log(result);
-//     });
-
-//     correios.consultaCEP({ cep: '30140082' }, (err, result) => {
-//         console.log(result);
-//     })
-
-// }
 
