b31ffd827ebb2ba1c2ab7be25156d3ace8295d25
Caciquez <victorcaciquinho@gmail.com>
2018-11-28 21:56:11 -0200

    - DashBoard controller new Features - Changes to Payment Controller - User controller new Features - Routes aswell alteraded.

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -7,19 +7,38 @@ const mail = require('../wrappers/mail');
 
 
 exports.getPurchases = (req, res, callback) => {
+    const page = req.params.page;
+
     const query = `
-        select * from purchases ORDER by created_at DESC
+        SELECT * FROM purchases 
+        ORDER by created_at DESC LIMIT 15 OFFSET ($1);
         `;
 
-    POOL.query(query).then(result => {
-        console.log(result.rows);
-
-
-        //if (result.rows.length > 0) return callback(null, 200, result.rows);
+    POOL.query(query, [page]).then(result => {
+        if (result.rows.length > 0) return callback(null, 200, result.rows);
     }).catch(err => { return callback(err, 500); })
 };
 
-exports.getTotalSoldThisMonth
+exports.getTotalSoldByDate = (req, res, callback) => {
+
+    //     -- dashboard info querys
+    const queryFee = `
+    SELECT SUM(transaction_fee) FROM purchases 
+    WHERE created_at BETWEEN ($1) 
+    AND ($2)`;
+
+
+    const queryTotal = `
+    SELECT SUM((sale -> 'amount' ->> 'total')::numeric) 
+    FROM purchases WHERE created_at < $(1)
+    `;
+
+    const queryQuantity = `
+    SELECT name, SUM(quantity) 
+    FROM item_purchases
+    GROUP BY name
+    `;
+};
 
 
 exports.sendCode = (req, res, callback) => {
